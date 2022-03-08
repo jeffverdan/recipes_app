3511f6f2b3b787349897ec126efe2e84bc01b2fa
Jeferson Verdan <jeff_verdan@outlook.com>
2022-03-08 00:26:52 -0300

    Requisito Header Ok

---
+++ src/pages/ExploreFoodsArea.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../components/Header';
+
+export default function ExploreFoodsArea() {
+  return (
+    <div>
+      <Header title="Explore Nationalities" loadingSearch />
+    </div>
+  );
+}
