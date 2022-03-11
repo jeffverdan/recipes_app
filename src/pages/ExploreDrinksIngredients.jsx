b19a88d03f756ce2e52e4601a7c7ce120a91ed96
Jonathan Plets <jonathanplets@gmail.com>
2022-03-11 12:49:01 -0300

    requisito 75 e 76

--- src/pages/ExploreDrinksIngredients.jsx
+++ src/pages/ExploreDrinksIngredients.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function ExploreDrinksIngredients() {
+  const [ingredientsList, setIngredients] = useState('');
+  const NUM = 12;
+  const fetchApiJson = async (url) => {
+    const results = await fetch(url).then((response) => response.json());
+    setIngredients(results.drinks);
+  };
+
+  useEffect(() => {
+    fetchApiJson('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list');
+  }, []);
+
   return (
     <div>
       <Header title="Explore Ingredients" />
+
+      {ingredientsList && ingredientsList.slice(0, NUM).map((ingredient, index) => (
+        <div key={ ingredient.strIngredient1 } data-testid={ `${index}-ingredient-card` }>
+          <img
+            src={ `https://www.thecocktaildb.com/images/ingredients/${ingredient.strIngredient1}-Small.png` }
+            alt={ ingredient.strIngredient1 }
+            data-testid={ `${index}-card-img` }
+          />
+          <p data-testid={ `${index}-card-name` }>{ ingredient.strIngredient1 }</p>
+        </div>
+      ))}
       <Footer />
     </div>
   );
--- src/pages/ExploreFoodsIngredients.jsx
+++ src/pages/ExploreFoodsIngredients.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
-import Header from '../components/Header';
+import React, { useState, useEffect } from 'react';
 import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 export default function ExploreFoodsIngredients() {
+  const [ingredientsList, setIngredients] = useState('');
+  const NUM = 12;
+  const fetchApiJson = async (url) => {
+    const results = await fetch(url).then((response) => response.json());
+    setIngredients(results.meals);
+  };
+
+  useEffect(() => {
+    fetchApiJson('https://www.themealdb.com/api/json/v1/1/list.php?i=list');
+  }, []);
+
   return (
     <div>
       <Header title="Explore Ingredients" />
+      {ingredientsList && ingredientsList.slice(0, NUM).map((ingredient, index) => (
+        <div key={ ingredient.idIngredient } data-testid={ `${index}-ingredient-card` }>
+          <img
+            src={ `https://www.themealdb.com/images/ingredients/${ingredient.strIngredient}-Small.png` }
+            alt={ ingredient.strIngredient }
+            data-testid={ `${index}-card-img` }
+          />
+          <p data-testid={ `${index}-card-name` }>{ ingredient.strIngredient }</p>
+        </div>
+      ))}
       <Footer />
     </div>
   );
