fad255b76efd543620bad77c1d9979b175b92212
Filipe <filipe.b.braga@gmail.com>
2022-03-13 18:38:04 -0300

    req 54 a 58. Menos 57

--- src/pages/RecipesDone.jsx
+++ src/pages/RecipesDone.jsx
@@ -1,10 +1,47 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Header from '../components/Header';
 import DoneCard from '../components/DoneCard';
 import AppContext from '../context/AppContext';
 
 export default function RecipesDone() {
   const { doneRecepie } = useContext(AppContext);
+  const [doneRecepieState, setDoneRecepieState] = useState([]);
+  // const test = doneRecepie[0].meals;
+  // console.log(doneRecepie[0].meals);
+  console.log(doneRecepie);
+
+  useEffect(() => {
+    setDoneRecepieState(doneRecepie);
+  }, []);
+
+  const handleClickAll = () => {
+    // Using reduce to filter Name
+    setDoneRecepieState(doneRecepie);
+  };
+
+  const handleClickFood = () => {
+    // Using reduce to filter Name
+    const doneRecepieReduce = doneRecepie.reduce((filtered, recepie) => {
+      if (recepie.idMeal !== undefined) {
+        filtered.push(recepie);
+      }
+      return filtered;
+    }, []);
+    console.log(doneRecepieReduce);
+    setDoneRecepieState(doneRecepieReduce);
+  };
+
+  const handleClickDrink = () => {
+    // Using reduce to filter Name
+    const doneRecepieReduce = doneRecepie.reduce((filtered, recepie) => {
+      if (recepie.idDrink !== undefined) {
+        filtered.push(recepie);
+      }
+      return filtered;
+    }, []);
+    console.log(doneRecepieReduce);
+    setDoneRecepieState(doneRecepieReduce);
+  };
 
   return (
     <div>
@@ -14,7 +51,7 @@ export default function RecipesDone() {
           data-testid="filter-by-all-btn"
           value="all"
           type="button"
-        // onClick={ handleClick }
+          onClick={ handleClickAll }
         >
           All
         </button>
@@ -22,7 +59,7 @@ export default function RecipesDone() {
           data-testid="filter-by-food-btn"
           value="food"
           type="button"
-        // onClick={ handleClick }
+          onClick={ handleClickFood }
         >
           Food
         </button>
@@ -30,15 +67,16 @@ export default function RecipesDone() {
           data-testid="filter-by-drink-btn"
           value="drink"
           type="button"
-        // onClick={ handleClick }
-        // onClick={ handleClick }
+          onClick={ handleClickDrink }
         >
           Drinks
         </button>
       </div>
+      {/* {doneRecepie.map((item, index) => (console.log(item.meals[index].strTags.split(',')))) } */}
+      {/* {const test = doneRecepie[0].meals)} */}
       { doneRecepie !== undefined
       && doneRecepie !== null
-      && doneRecepie.map((item, index) => (
+      && doneRecepieState.map((item, index) => (
         // <div key={ item.idMeal }>
         //   <h1>{item.strMeal}</h1>
         //   <img src={ item.strMealThumb } alt={ item.strMeal } />
@@ -46,12 +84,14 @@ export default function RecipesDone() {
         <div className="containerMainCard" key={ item.idMeal }>
           <DoneCard
             /* key={ item.idMeal } */
-            idMeal={ item.idMeal }
-            strMeal={ item.strMeal }
-            strMealThumb={ item.strMealThumb }
+            idMeal={ item.idMeal ? item.idMeal : item.idDrink }
+            strMeal={ item.strMeal ? item.strMeal : item.strDrink }
+            strMealThumb={ item.strMealThumb ? item.strMealThumb : item.strDrinkThumb }
             index={ index }
-            strTags={ item.strTags }
-            strCategory={ item.strCategory }
+            strTags={ item.strTags ? item.strTags.split(',') : null }
+            strCategory={ `${item.strArea ? item.strArea : 'Alcoholic'
+            } - ${item.strCategory}` }
+            type={ item.idMeal ? 'foods' : 'drinks' }
           />
         </div>
       ))}
